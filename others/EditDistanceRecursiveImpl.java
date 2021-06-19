c246d4ca6518487db3aac3340ff3a41fc1e26a34
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2021-06-19 17:39:40 -0600

    Update EditDistanceRecursiveImpl.java

--- others/EditDistanceRecursiveImpl.java
+++ others/EditDistanceRecursiveImpl.java
@@ -16,15 +16,15 @@ We know that at the end of the transformation, both Strings will be of equal len
 
 Substitution:
 Determine the cost (D1) of substituting x[1] with y[1]. The cost of this step would be zero if both characters are same. If not, then the cost would be one
-After step 1.1, we know that both Strings start with the same character. Hence the total cost would now be the sum of the cost of step 1.1 and the cost of transforming the rest of the String x[2:m] into y[2:n]
+After the above step, we know that both Strings start with the same character. Hence the total cost would now be the sum of the cost of step 1.1 and the cost of transforming the rest of the String x[2:m] into y[2:n]
 
 Insertion:
 Insert a character in x to match the first character in y, the cost of this step would be one
-After 2.1, we have processed one character from y. Hence the total cost would now be the sum of the cost of step 2.1 (i.e., 1) and the cost of transforming the full x[1:m] to remaining y (y[2:n])
+After the above step, we have processed one character from y. Hence the total cost would now be the sum of the cost of step 2.1 (i.e., 1) and the cost of transforming the full x[1:m] to remaining y (y[2:n])
 
 Deletion:
 Delete the first character from x, the cost of this step would be one
-After 3.1, we have processed one character from x, but the full y remains to be processed. The total cost would be the sum of the cost of 3.1 (i.e., 1) and the cost of transforming remaining x to the full y
+After the above step, we have processed one character from x, but the full y remains to be processed. The total cost would be the sum of the cost of 3.1 (i.e., 1) and the cost of transforming remaining x to the full y
 
 The next part of the solution is to figure out which option to choose out of these three. Since we do not know which option would lead to minimum cost at the end, we must try all options and choose the best one.
 
