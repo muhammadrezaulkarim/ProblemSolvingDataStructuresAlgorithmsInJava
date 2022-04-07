cabdb1fba2fbfd1eafc6cc19bafa4092ca62dbae
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2022-04-06 20:59:10 -0600

    Update StackUse.java

--- others/StackUse.java
+++ others/StackUse.java
@@ -5,9 +5,13 @@ public class StackUse
 	{
 		Stack<Integer> st=new Stack<Integer>(); 
 		
-		st.add(1);
-		st.add(2);
-		st.add(-1);
+		st.push(1); //Pushes an item onto the top of this stack
+		st.push(2);
+		st.push(-1);  
+		
+		st.pop(); //Removes the object at the top of this stack and returns that object as the value of this function.
+		
+		st.search(-1); // returns how far an item is from the top. the topmost item on the stack is considered to be at distance 1.
 		
 		// check whether the stack is empty or not
 		//st.isEmpty()
@@ -15,8 +19,11 @@ public class StackUse
 		 st.clear();
 		
 		//System.out.println(st.peek()); // inspect the top of the stack but do not remove it
+		
 		System.out.println(st.size()); 
 		System.out.println(st.contains(-1)); 
+		
+		
 		
 		// StringBuilder Example
 		StringBuilder sb = new StringBuilder();
