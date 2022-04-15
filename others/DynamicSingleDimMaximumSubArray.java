c4430bc4ef3caceacb78300d919dd6939c9bdd4f
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2022-04-15 17:02:36 -0600

    Update DynamicSingleDimMaximumSubArray.java

--- others/DynamicSingleDimMaximumSubArray.java
+++ others/DynamicSingleDimMaximumSubArray.java
@@ -1,67 +1,49 @@
 import java.util.Arrays;
 
-public class DynamicSingleDimMaximumSubArray  {
+public class DynamicSingleDimMaximumSubArray {
 
-    //bottom up approach
-    public int solve(int [] a){
-        int [] solution = new int[a.length+1];
-        solution[0] = 0;
+    // bottom up approach
+    public int solve(int[] a) {
+        int[] solution = new int[a.length];
+        solution[0] = a[0];
 
-        for (int j = 1; j <solution.length ; j++) 
-        {
-            solution[j] = Math.max(solution[j-1] + a[j-1] , a[j-1]);
+        for (int j = 1; j < solution.length; j++) {
+            solution[j] = Math.max(solution[j - 1] + a[j], a[j]);
         }
-        
-        //this will print the solution matrix
-        //System.out.println(Arrays.toString(solution));
-        //now return the maximum in the solution arrayyy
+
+        // this will print the solution matrix
+        // System.out.println(Arrays.toString(solution));
+        // now return the maximum in the solution array
         int result = solution[0];
-        for (int j = 1; j <solution.length ; j++) 
-        {
-            if(result<solution[j])
+        for (int j = 1; j < solution.length; j++) {
+            if (solution[j] > result)
                 result = solution[j];
         }
 
         return result;
     }
 
-    public static void main(String[] args) 
-    {
-        int array[] = { 1, 2, -3, -4, 2, 7, -2, 3 };
+    public static void main(String[] args) {
+        int array[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
         DynamicSingleDimMaximumSubArray ob = new DynamicSingleDimMaximumSubArray();
         System.out.println("Maximum subarray is  " + ob.solve(array));
     }
 }
 
 /*
-
-Objective:  The maximum subarray problem is the task of finding the contiguous subarray within 
-a one-dimensional array of numbers which has the largest sum.
-
-
-Example:
-
-int [] A = {−2, 1, −3, 4, −1, 2, 1, −5, 4};
-
-Output: contiguous subarray with the largest sum is 4, −1, 2, 1, with sum 6.
-
-
-The problem will be solved in bottom-up manner.
-
-let’s say
-
-“MS(i)  is maximum sum ending at index i” 
-
-
-Recursive Solution:
-
-MS(i) = Max[MS(i-1) + A[i] , A[i]]
-
-
-To calculate the solution for any element at index “i” has two options:
-
-1. added to the solution found till “i-1“th index
-OR 
-2. start a new sum from the index “i“.  
-
-*/
+ * Objective: The maximum subarray problem is the task of finding the contiguous
+ * subarray within
+ * a one-dimensional array of numbers which has the largest sum.
+ * Example:
+ * int [] A = {−2, 1, −3, 4, −1, 2, 1, −5, 4};
+ * Output: contiguous subarray with the largest sum is 4, −1, 2, 1, with sum 6.
+ * The problem will be solved in bottom-up manner.
+ * let’s say
+ * “MS(i) is maximum sum ending at index i”
+ * Recursive Solution:
+ * MS(i) = Max[MS(i-1) + A[i] , A[i]]
+ * To calculate the solution for any element at index “i” has two options:
+ * 1. added to the solution found till “i-1“th index
+ * OR
+ * 2. start a new sum from the index “i“.
+ */
