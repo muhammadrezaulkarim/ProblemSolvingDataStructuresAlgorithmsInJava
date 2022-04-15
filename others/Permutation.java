32fcc7c8ab7218835781d26994c0daeafcf3a931
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2022-04-15 14:18:14 -0600

    Update Permutation.java

--- others/Permutation.java
+++ others/Permutation.java
@@ -1,207 +1,148 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Permutation 
 {
-	static String number1, number2;
-    static ArrayList<Integer> number1List = new ArrayList<Integer>(), number2List = new ArrayList<Integer>();
-     
-	static int [] num1, num2;
-	static Scanner sc=null;
-	
-	static Boolean [] flag ;
-    static Boolean foundFlag = false;
-	
-	static int[] permutationHolder;
-    static char[] permutationCharHolder;
-	
+	private static String inputStr; 
+	private static Boolean [] flag ;
+
+	private StringBuilder permutationHolder = new StringBuilder();
+
 	public static void main(String args[])
 	{
+        Scanner sc = null;
+
 		try
 		{
-			// foundFlag = false;
-			sc=new Scanner(System.in);
-			number1=sc.nextLine();
-			number2=sc.nextLine();
-		    GenerateAndCheckWithPermutation();
-		    
-		    // if (foundFlag == true)
-            //   System.out.println("YES");
-           //else
-              // System.out.println("NO"); 
-			
+			sc = new Scanner(System.in);
+			inputStr = sc.nextLine();
+		   
+            flag = new Boolean[inputStr.length()];
+
+            for (int i = 0; i < flag.length; i++)
+            {
+                flag[i] = false;
+            }
+
+            System.out.println();
+
+            Permutation permGenerator = new Permutation();
+
+            // requires to search for a sepecific string
+           // boolean found = false;
+
+            for (int i = 0; i < inputStr.length(); i++)
+            {
+
+               permGenerator.permuteChar(i);
+
+               /*
+               // search for a sepcific string
+               found = permGenerator.findStringWithPermuation(i, "dbca");
+
+               if(found)
+               {
+                   System.out.println("found");
+                   break;
+               } */
+            }		
 		}
 		catch(Exception ex)
 		{
 			
 		}
-
+        finally {
+            if(sc != null) {
+                sc.close();
+            }
+        }
 	}
-	
-    public static void GenerateAndCheckWithPermutation()
-    {
-        char[] cr = new char[1];
-        cr[0] = ' ';
 
-        String[] inputNumbers1 = number1.split(" ");
-        String[] inputNumbers2 = number2.split(" ");
-
-        for (int i = 0; i < inputNumbers1.length; i++)
-        {
-            if (!inputNumbers1.equals(" "))
-                number1List.add(Integer.parseInt(inputNumbers1[i]));
-
-            if (!inputNumbers2.equals(" "))
-            	 number2List.add(Integer.parseInt(inputNumbers2[i]));
-        }
-
-
-        
-        num1 = new int [number1List.size()];
-        num2 = new int [number2List.size()];
-        
-        for(int i=0;i<num1.length;i++)
-        {
-        	  num1[i]=number1List.get(i);
-        	  num2[i]=number2List.get(i);
-        }
-
-
-        // permuteDigits(num1, 0, num1.Length - 1);
-
-      flag = new Boolean[num1.length];
-      permutationHolder = new int[num1.length];
-
-      for (int i = 0; i < flag.length; i++)
-      {
-            flag[i] = false;
-      }
-        // only required if we are matching with a specific number
-       // foundFlag = false;  
-        for (int i = 0; i < num1.length; i++)
-        {
-            //	second parameter level=0;
-            permuteDigit(i, 0);
-           // permuteWithNumber(i, 0);
-            
-            // only required if we are matching with a specific number
-           // if (foundFlag == true)
-            // break;
-        }
-
-    }
-  /* //number 1 is a string 
-    public static void permuteChar(int k, int posInNextPerm)
+    public void permuteChar(int k)
     {
         //permutationHolder: holds the next permutation
         flag[k] = true; //at the beginning set the flag to true
-        permutationCharHolder[posInNextPerm] = number1.charAt(k);
-        posInNextPerm++;
+        permutationHolder.append(inputStr.charAt(k));
 
-        if (posInNextPerm == number1.length())
-        {
-            
-            flag[k] = false; //before returning set the flag to false
+        int currentLength = permutationHolder.toString().length();
 
-            System.out.println();
-            for (int l = 0; l < permutationCharHolder.length; l++)
-                System.out.print(permutationCharHolder[k] + " ");
+        if (currentLength == inputStr.length())
+        {     
+            System.out.println(permutationHolder.toString());
 
-            return; // we have reached the limit of nextPermutation index. need to backtrack
+            //before returning set the flag to false so that the symbol can be reached again in the search
+            flag[k] = false; 
+
+            // backtrack: remove the character appended in this call to the current permuation.
+            permutationHolder.delete(currentLength-1, currentLength); // end index exclusive
+
+            return; // need to backtrack
         }
 
 
-        for (int i = 0; i < number1.length(); i++)
+        for (int i = 0; i < inputStr.length(); i++)
         {
             if (flag[i] == false)
             {
-                //permuteDigit(i, posInNextPerm);
-                permuteWithNumber(i, posInNextPerm);
+                permuteChar(i);
             }
         }
 
-        flag[k] = false; //before returning set the flag to false
-    } */
+        //before returning set the flag to false so that the symbol can be reached again in the search
+        flag[k] = false; 
+
+        // backtrack: remove the character appended in this call to the current permuation.
+        permutationHolder.delete(currentLength-1, currentLength); // end index exclusive
+    } 
+
     
-    public static void permuteDigit(int k, int posInNextPerm)
+    public boolean findStringWithPermuation(int k, String targetStr)
     {
         //permutationHolder: holds the next permutation
         flag[k] = true; //at the beginning set the flag to true
-        permutationHolder[posInNextPerm] = num1[k];
-        posInNextPerm++;
+        permutationHolder.append(inputStr.charAt(k));
 
-        if (posInNextPerm == num1.length)
-        {
-            
-            flag[k] = false; //before returning set the flag to false
+        int currentLength = permutationHolder.toString().length();
 
-            System.out.println();
-            for (int l = 0; l < permutationHolder.length; l++)
-                System.out.print(permutationHolder[l] + " ");
+        if (currentLength == inputStr.length())
+        {     
 
-            return; // we have reached the limit of nextPermutation index. need to backtrack
-        }
+            //before returning set the flag to false so that the symbol can be reached again in the search
+            flag[k] = false; 
 
-
-        for (int i = 0; i < num1.length; i++)
-        {
-            if (flag[i] == false)
+            if(permutationHolder.toString().equals(targetStr)) 
             {
-                permuteDigit(i, posInNextPerm);
-                //permuteWithNumber(i, posInNextPerm);
+                // backtrack: remove the character appended in this call to the current permuation.
+                permutationHolder.delete(currentLength-1, currentLength); // end index exclusive
+
+                return true;
+            }
+            else
+            {
+                // backtrack: remove the character appended in this call to the current permuation.
+                permutationHolder.delete(currentLength-1, currentLength); // end index exclusive
+
+                return false;
             }
         }
 
-        flag[k] = false; //before returning set the flag to false
+
+        for (int i = 0; i < inputStr.length(); i++)
+        {
+            if (flag[i] == false)
+            {
+                boolean found = findStringWithPermuation(i, targetStr);
+                
+                if(found) // prune the search space
+                    return true;
+            }
+        }
+
+        //before returning set the flag to false so that the symbol can be reached again in the search
+        flag[k] = false; 
+
+        // backtrack: remove the character appended in this call to the current permuation.
+        permutationHolder.delete(currentLength-1, currentLength); // end index exclusive
+
+        return false;
     }
-    
-
-    public static void permuteWithNumber(int k, int posInNextPerm)
-    {
-        //permutationHolder: holds the next permutation
-        // only required if we are matching with a specific number
-        // if (foundFlag == true)
-        // return;
-
-        flag[k] = true;
-        permutationHolder[posInNextPerm] = num1[k];
-        posInNextPerm++;
-
-        if (posInNextPerm == num1.length)
-        {
-
-            flag[k] = false; //before returning set the flag to false
-            
-            // print the permutation
-
-            System.out.println();
-            for (int l = 0; l < permutationHolder.length; l++)
-            	 System.out.print(permutationHolder[l] +" " );
-
-            // This part is required if we are matching with a specific number
-            /* int matchCount = 0;
-                          for (int l = 0; l < permutationHolder.length; l++)
-                              if (permutationHolder[l] == num2[l])
-                                  matchCount++;
-                          if (matchCount == num2.length)
-                          {
-                              foundFlag = true;
-                        
-                          } */
-
-            return;  // we have reached the limit of nextPermutation index. need to backtrack
-        }
-
-
-        for (int i = 0; i < num1.length; i++)
-        {
-            if (flag[i] == false)
-            {
-                permuteWithNumber(i, posInNextPerm);
-            }
-        }
-
-        flag[k] = false; //before returning set the flag to false
-     }
-
 }
