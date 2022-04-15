a3cddc22608f3db3e97e997915ddfabc05275c08
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2022-04-15 16:24:34 -0600

    Update DynamicSubsetSum.java

--- others/DynamicSubsetSum.java
+++ others/DynamicSubsetSum.java
@@ -2,62 +2,49 @@
 //Find whether or not there exists any subset of the given set, 
 //the sum of whose elements is equal to the given value of sum
 
+public class DynamicSubsetSum {
+    public static boolean[][] solutionExists = null;
 
-public class DynamicSubsetSum 
-{
-	public static boolean [][] solutionExists = null;
-	
-    public static void main(String[] args) 
-    {
-    	int array[] = {92, 40, 63, 51, 36,41,41,42};
-    	int targetSum = 154;
-    	
-    
-    	
-    	DynamicSubsetSum  ob = new DynamicSubsetSum();
-        System.out.println(ob.subsetSum(array, array.length,targetSum));
+    public static void main(String[] args) {
+        int array[] = { 92, 40, 63, 51, 36, 41, 41, 42 };
+        int targetSum = 154;
+
+        DynamicSubsetSum ob = new DynamicSubsetSum();
+        System.out.println(ob.subsetSum(array, targetSum));
     }
-    
-    boolean subsetSum(int array[],int n, int targetSum)
-    {
-    	solutionExists = new boolean[n +1][targetSum+1];
-    	
-        //solutionExists[i][j] indicates whether there is a subset of sum j in the subarray array[0....i-1]
-     
-        int i=0,j=0;
-     
-        //for sum=0, there is always a subset possible (the empty set)
-        for(i=0;i<=n;i++)
-        	solutionExists[i][0]=true;
-     
-        //if there are no elements in the array, no subset is possible for a non-zero sum
-        for(j=1;j<=targetSum;j++)
-        	solutionExists[0][j]=false;
-     
-        //i represents the number of elements of array considered
-        for(i=1;i<=n;i++)
-        {
-            //j represents the sum of subset being searched for
-            for(j=1;j<=targetSum;j++)
-            {
-                //if using i-1 elements, there is a subset of desired sum no need to search further
-                if(solutionExists[i-1][j]==true)
-                	solutionExists[i][j]=true;
-     
+
+    boolean subsetSum(int array[], int targetSum) {
+        int n = array.length;
+
+        solutionExists = new boolean[n + 1][targetSum + 1];
+
+        // solutionExists[i][j] indicates whether sum j can be attained
+        // using items up to first `i` items in the array
+
+        int i = 0, j = 0;
+
+        // for sum=0, there is always a subset possible (the empty set)
+        for (i = 0; i <= n; i++)
+            solutionExists[i][0] = true;
+
+        // i represents the number of elements of array considered
+        for (i = 1; i <= n; i++) {
+            // j represents the sum of subset being searched for
+            for (j = 1; j <= targetSum; j++) {
+
+                // if value of i'th item is greater than the required sum
+                // this element cannot be considered
+                if (array[i - 1] > j)
+                    solutionExists[i][j] = solutionExists[i - 1][j];
                 else
-                {
-                    //if value of current element is greater than the required sum
-                    //this element cannot be considered
-                    if(array[i-1]>j)
-                    	solutionExists[i][j]=false;
-                        //check that after including this element, Is there any subset present for the remaining sum ie., j-array[i-1]
-                    else
-                    	solutionExists[i][j]=solutionExists[i-1][j-array[i-1]];
-                }
+                    // find the subset with sum `j` by excluding or including
+                    // the i'th item
+                    solutionExists[i][j] = solutionExists[i - 1][j] || solutionExists[i - 1][j - array[i - 1]];
+
             }
         }
-     
-        //return the overall result
+
+        // return the overall result
         return solutionExists[n][targetSum];
     }
 
