54838f32e0fc4c7c253d5399beac9ed6445283c8
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2022-05-02 14:03:27 -0600

    Update DFSMatrixMinSteps.java

--- others/DFSMatrixMinSteps.java
+++ others/DFSMatrixMinSteps.java
@@ -1,160 +1,95 @@
 import java.util.*;
 
 public class DFSMatrixMinSteps {
+    int nRows, nCols, destRow, destCol;
+    int[][] minEffort;
 
-    boolean flag[][] = null;
-    int n_row = 0;
-    int n_col = 0;
+    public int minimumEffortPath(String[] inputArray) {
+        nRows = inputArray.length;
+        nCols = inputArray[0].length();
 
-    // StringBuilder sb=new StringBuilder();
+        minEffort = new int[nRows][nCols];
 
-    // sample input 1
-    // String [] inputArray = {"11000","01100","00501","10001","01011"};
+        // effort needed to reach from [0,0] to current cell
+        for (int i = 0; i < nRows; i++) {
+            for (int j = 0; j < nCols; j++) {
+                minEffort[i][j] = Integer.MAX_VALUE;
 
-    // sample input 2
-    // String [] inputArray = {"11000","11100","51001","10001","01011"};
-
-    // sample input 3
-    String[] inputArray = { "11000", "11100", "11151", "10001", "01011" };
-
-    Queue<Node> queue = new LinkedList<Node>();
-
-    public void run() {
-
-        n_row = inputArray.length;
-        n_col = inputArray[0].length();
-
-        flag = new boolean[n_row][n_col];
-
-        // initilize the visit flag
-
-        for (int i = 0; i < n_row; i++)
-            for (int j = 0; j < n_row; j++)
-                flag[i][j] = false;
-
-        // enable these two lines for BFS search
-        Node root = new Node(0, 0, 0);
-        int minSteps = BFS(root);
-
-        // int minSteps = DFS(0, 0, 0, Integer.MAX_VALUE);
-        System.out.println("Minimum Steps: " + minSteps);
-    }
-
-    public int DFS(int row, int col, int depth, int minSteps) {
-
-        flag[row][col] = true;
-
-        if (inputArray[row].charAt(col) == '5') {
-            if (depth < minSteps) {
-                minSteps = depth;
-                // make the cell containing '5' false
-                // so that it can be visited from other nodes again
-                // not part of normal DFS search
-                // special tweek for this search problem
-
-                flag[row][col] = false;
-                return minSteps;
-            }
-
-        }
-
-        int[] addRow = { -1, 1, 0, 0 };
-        int[] addCol = { 0, 0, -1, 1 };
-
-        // find adjacent cells including the destination cell containg '5'
-        for (int i = 0; i < addRow.length; i++) {
-            int nextRow = row + addRow[i];
-            int nextCol = col + addCol[i];
-
-            if (nextRow >= 0 && nextRow < n_row && nextCol >= 0 && nextCol < n_col) {
-                // destination cell '5' is considered as adjacent cell as well
-                if ((inputArray[nextRow].charAt(nextCol) == '1' || inputArray[nextRow].charAt(nextCol) == '5')
-                        && !flag[nextRow][nextCol]) {
-                    int steps = DFS(nextRow, nextCol, depth + 1, minSteps);
-
-                    if (steps < minSteps)
-                        minSteps = steps;
+                if (inputArray[i].charAt(j) == '5') {
+                    destRow = i;
+                    destCol = j;
                 }
             }
-
         }
 
-        // for graph search, the same node can be reached from
-        // different parent nodes. so for DFS set it to false so that
-        // it can be reached from different parent nodes
-        flag[row][col] = false;
+        minEffort[0][0] = 0; // set for start cell
 
-        return minSteps;
-
-    }
-
-    public int BFS(Node root) {
-        int minSteps = Integer.MAX_VALUE;
-
-        queue.add(root);
-        flag[root.row][root.col] = true;
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(new Node(0, 0));
 
         while (!queue.isEmpty()) {
             Node node = queue.remove();
 
-            if (inputArray[node.row].charAt(node.col) == '5') {
-                if (node.level < minSteps) {
-                    minSteps = node.level;
-                    // make the cell containing '5' false
-                    // so that it can be visited from other nodes again
-                    // not part of normal DFS search
-                    // special tweek for this search problem
-                    flag[node.row][node.col] = false;
+            System.out.println("current node. row:" + node.row + " col:" + node.col);
 
-                }
+            int[] rowOffsets = { 0, 0, -1, 1 };
+            int[] colOffsets = { -1, 1, 0, 0 };
 
-            }
+            for (int i = 0; i < rowOffsets.length; i++) {
+                int nextRow = node.row + rowOffsets[i];
+                int nextCol = node.col + colOffsets[i];
 
-            int[] addRow = { -1, 1, 0, 0 };
-            int[] addCol = { 0, 0, -1, 1 };
+                if (isWithinBounds(nextRow, nextCol)
+                        && (inputArray[nextRow].charAt(nextCol) == '1' || inputArray[nextRow].charAt(nextCol) == '5')) {
+                    int currentCellEffort = minEffort[node.row][node.col] + 1;
 
-            // find adjacent
-            for (int i = 0; i < addRow.length; i++) {
-                int nextRow = node.row + addRow[i];
-                int nextCol = node.col + addCol[i];
-
-                if (nextRow >= 0 && nextRow < n_row && nextCol >= 0 && nextCol < n_col) {
-                    // destination cell '5' is considered as adjacent cell as well
-                    // otherwise we cannot inspect it
-                    if (inputArray[nextRow].charAt(nextCol) == '1' || inputArray[nextRow].charAt(nextCol) == '5') {
-                        if (!flag[nextRow][nextCol]) // if not visited
-                        {
-                            Node child = new Node(nextRow, nextCol, node.level + 1);
-                            flag[node.row][node.col] = true;
-                            queue.add(child);
-                        }
+                    // we found better path (path that takes less effort)
+                    // helps to prune the search space
+                    if (currentCellEffort < minEffort[nextRow][nextCol]) {
+                        minEffort[nextRow][nextCol] = currentCellEffort;
+                        System.out.println("added node. row:" + nextRow + " col:" + nextCol);
+                        queue.add(new Node(nextRow, nextCol));
                     }
+
                 }
 
             }
-
         }
 
-        return minSteps;
+        return minEffort[destRow][destCol];
+    }
+
+    public boolean isWithinBounds(int row, int col) {
+        if (row >= 0 && row < nRows && col >= 0 && col < nCols)
+            return true;
+
+        return false;
 
     }
 
     public static void main(String args[]) {
+        // sample input 1
+        String[] inputArray = { "11000", "01100", "00501", "10001", "01011" };
+
+        // sample input 2
+        // String [] inputArray = {"11000","11100","51001","10001","01011"};
+
+        // sample input 3
+        // String [] inputArray = {"11000","11100","11151","10001","01011"};
+
         DFSMatrixMinSteps ob = new DFSMatrixMinSteps();
-        ob.run();
+        System.out.println(ob.minimumEffortPath(inputArray));
     }
 
-    class Node {
-        int row;
-        int col;
-        int level;
+    private class Node {
+        int row, col;
 
-        public Node(int row, int col, int level) {
+        public Node(int row, int col) {
             this.row = row;
             this.col = col;
-            this.level = level;
         }
     }
+
 }
 
 /*
