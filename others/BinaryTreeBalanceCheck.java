750c7930cb0cc9bc9745877868cb3c46d6728ebe
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2024-05-05 15:18:39 -0600

    Update BinaryTreeBalanceCheck.java

--- others/BinaryTreeBalanceCheck.java
+++ others/BinaryTreeBalanceCheck.java
@@ -1,68 +1,87 @@
 public class BinaryTreeBalanceCheck {
 
-	// This function prints all paths that have sum k
-	public int heightBalanced(Node node)
-	{
-		// empty node
-		if (node == null)
-			return 0; //leaf node has height 0
+	Node root;
 
-		int leftSubtreeHeight = heightBalanced(node.left);
-		if (leftSubtreeHeight == Integer.MIN_VALUE)
-			return Integer.MIN_VALUE;  // left subtree tree unbalanced. return without futher proceeding
+	// Check height balance
+	boolean checkHeightBalance(Node node) {
 
-		int rightSubtreeHeight = heightBalanced(node.right);
-		if (rightSubtreeHeight == Integer.MIN_VALUE)
-			return Integer.MIN_VALUE;  // right subtree tree unbalanced. return without futher proceeding
+		// leaf node
+		if (node.left == null && node.right == null) {
+			node.height = 0;
+			return true;
+		}
 
-		
-		if (Math.abs(leftSubtreeHeight - rightSubtreeHeight) > 1) // unbalanced tree
-			return Integer.MIN_VALUE;  // signal tree unbalanced.
+		if (node.left != null && node.right != null) {
+			boolean leftBalanced = checkHeightBalance(node.left);
+			boolean rightBalanced = checkHeightBalance(node.right);
+
+			int diff = Math.abs(node.left.height - node.right.height);
+
+			if (!leftBalanced || !rightBalanced || diff > 1)
+				return false;
+
+			node.height = node.left.height > node.right.height ? (node.left.height + 1) : (node.right.height + 1);
+		}
+
+		else if (node.left != null) {
+			boolean leftBalanced = checkHeightBalance(node.left);
+
+			// tree not balanced
+			if (!leftBalanced || node.left.height > 1)
+				return false;
+
+			node.height = node.left.height + 1;
+		}
+
+		else if (node.right != null) {
+			boolean rightBalanced = checkHeightBalance(node.right);
+
+			// tree not balanced
+			if (!rightBalanced || node.right.height > 1)
+				return false;
+
+			node.height = node.right.height + 1;
+		}
+
+		return true;
+	}
+
+	public void run() {
+		root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+
+		if (checkHeightBalance(root))
+			System.out.println("The tree is balanced");
 		else
-			return Math.max(leftSubtreeHeight, rightSubtreeHeight) + 1; // return tree height
-
+			System.out.println("The tree is not balanced");
 	}
 
-	// A wrapper over printKPathUtil()
-	public boolean isTreeBalanced(Node root)
-	{
-		return heightBalanced(root) == Integer.MIN_VALUE? false: true;
+	public static void main(String args[]) {
+
+		BinaryTreeBalanceCheck tree = new BinaryTreeBalanceCheck();
+		tree.run();
 	}
 
-	public Node getTreeNode()
-	{
-		Node root = new Node(1);
-		root.left = new Node(3);
-		//root.left.left = new Node(2);  // enabling this node makes the tree balanced
-		root.left.right = new Node(1);
-		root.left.right.left = new Node(1);
-		root.right = new Node(-1);
-		root.right.left = new Node(4);
-		root.right.left.left = new Node(1);
-		root.right.left.right = new Node(2);
-		root.right.right = new Node(5);
-		root.right.right.right = new Node(2);
+	// Checking if a binary tree is height balanced in Java
 
-		return root;
-	}
+	// Node creation
+	class Node {
 
-	public static void main(String args[])
-	{
-
-		BinaryTreeBalanceCheck obj = new BinaryTreeBalanceCheck();
-		System.out.println(obj.isTreeBalanced(obj.getTreeNode()));
-	}
-
-	// binary tree node
-	private class Node {
 		int data;
 		Node left, right;
-		public Node(int data)
-		{
-			this.data = data;
-			left = right = null;
+		int height = -1;
+
+		Node(int d) {
+			data = d;
+			left = null;
+			right = null;
+			height = -1;
 		}
-	};
+	}
+
 }
 
 /*
