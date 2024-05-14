d7442f0bf20848952d0f3947e0870789c9df4fcb
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2024-05-14 15:26:16 -0600

    Create SlidingWindowLongestSubarrayFrequencyAtMostKLeetcode2958.java

---
+++ others/SlidingWindowLongestSubarrayFrequencyAtMostKLeetcode2958.java
@@ -0,0 +1,94 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SlidingWindowLongestSubarrayFrequencyAtMostKLeetcode2958 {
+    private Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+    public int maxSubarrayLength(int[] nums, int k) {
+        if (nums.length == 0 || k == 0)
+            return 0;
+
+        int start = 0, end = 0;
+        int maxlength = 0;
+
+        while (end < nums.length) {
+            int count = frequencyMap.getOrDefault(nums[end], 0) + 1;
+
+            if (count > k) {
+                //update the start pointer until we find the violating number
+                while (nums[start] != nums[end]) {
+                    // we need to reduce the count as this character will no be longer part of the subarray
+                    frequencyMap.put(nums[start], frequencyMap.get(nums[start]) - 1);
+                    start++;
+                }
+
+                // We need to move to the next index after the first occurence of the violating number
+                // This is where we next subarray will start
+                start++;
+
+            } else {
+                frequencyMap.put(nums[end], count);
+
+                maxlength = Math.max(maxlength, end - start + 1);
+            }
+
+            end++;
+        }
+
+        return maxlength;
+    }
+
+    public static void main(String[] args) {
+        SlidingWindowLongestSubarrayFrequencyAtMostKLeetcode2958 ob = new SlidingWindowLongestSubarrayFrequencyAtMostKLeetcode2958();
+
+        int[] nums = { 1,2,3,1,2,3,1,2};
+        int k = 2;
+
+        //int[] nums = { 1,2,1,2,1,2,1,2};
+        //int k = 1;
+        
+        // int[] nums = { 2, 2, 3 };
+        //int k = 1;
+
+       // int[] nums = { 1, 1, 1, 3 };
+        //int k = 2;
+
+        System.out.println(ob.maxSubarrayLength(nums, k));
+    }
+}
+/* 
+You are given an integer array nums and an integer k.
+The frequency of an element x is the number of times it occurs in an array.
+
+An array is called good if the frequency of each element in this array is less than or equal to k.
+Return the length of the longest good subarray of nums.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+Example 1:
+
+Input: nums = [1,2,3,1,2,3,1,2], k = 2
+Output: 6
+Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good.
+It can be shown that there are no good subarrays with length more than 6.
+
+Example 2:
+
+Input: nums = [1,2,1,2,1,2,1,2], k = 1
+Output: 2
+Explanation: The longest possible good subarray is [1,2] since the values 1 and 2 occur at most once in this subarray. Note that the subarray [2,1] is also good.
+It can be shown that there are no good subarrays with length more than 2.
+
+Example 3:
+
+Input: nums = [5,5,5,5,5,5,5], k = 4
+Output: 4
+Explanation: The longest possible good subarray is [5,5,5,5] since the value 5 occurs 4 times in this subarray.
+It can be shown that there are no good subarrays with length more than 4.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109  */
