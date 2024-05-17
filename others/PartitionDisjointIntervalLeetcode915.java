b55046f5b62e4e25bb41592a00cfef5ac9f9a70e
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2024-05-17 13:07:42 -0600

    Update PartitionDisjointIntervalLeetcode915.java

--- others/PartitionDisjointIntervalLeetcode915.java
+++ others/PartitionDisjointIntervalLeetcode915.java
@@ -1,39 +1,47 @@
 class PartitionDisjointIntervalLeetcode915 {
-  
-  public int partitionDisjoint(int[] nums) {
-    int [] minRight = new int[nums.length];
+    public int partitionDisjoint(int[] nums) {
+        int[] minRight = new int[nums.length];
 
-    // compute minimum upto each index starting from the end
-    minRight[nums.length - 1] = nums[nums.length - 1];
+        minRight[nums.length - 1] = nums[nums.length - 1];
+        // compute minimum upto each index starting from the end
+        // minRight[j] holds minimum of the values from minRight[j,j+1,j+2,...n]
+        for (int i = nums.length - 2; i >= 0; i--) {
+            minRight[i] = Math.min(minRight[i + 1], nums[i]);
+        }
 
-    for (int i = nums.length - 2; i >= 0; i--) {
-      minRight[i] = Math.min(minRight[i + 1], nums[i]);
+        int[] maxLeft = new int[nums.length];
+        maxLeft[0] = nums[0];
+        // compute max upto each index from the left
+        // maxLeft[j] holds maximum of the values from maxRight[0,1,...j]
+        for (int i = 1; i < nums.length; i++) {
+            maxLeft[i] = Math.max(maxLeft[i - 1], nums[i]);
+        }
+
+        int index = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            // Comparing maxLeft[i] with minRight[i + 1] is enough to
+            // ensure that every element in left partition is less than or equal to every
+            // element in right partition
+
+            // please note we compare index with index i+1 as we partition the array
+            if (maxLeft[i] <= minRight[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        return index + 1;
     }
 
-    int index = 0;
+    public static void main(String[] args) {
+        // int[] nums = { 5, 0, 3, 8, 6 };
+        // int[] nums = { 1, 1, 1, 0, 6, 12 };
+        int[] nums = { 6, 0, 8, 30, 37, 6, 75, 98, 39, 90, 63, 74, 52, 92, 64 };
 
-    int maxLeft = nums[0];
-    for (int i = 0; i < nums.length - 1; i++) {
-      // compute max upto this index from the left
-      maxLeft = Math.max(maxLeft, nums[i]);
-
-      if (maxLeft <= minRight[i + 1]) {
-        index = i;
-        break;
-      }
+        PartitionDisjointIntervalLeetcode915 ob = new PartitionDisjointIntervalLeetcode915();
+        System.out.println(ob.partitionDisjoint(nums));
     }
-
-    return index + 1;
-  }
-
-  public static void main(String[] args) {
-    int[] nums = { 5, 0, 3, 8, 6 };
-    // int[] nums = { 1, 1, 1, 0, 6, 12 };
-    // int[] nums = {6,0,8,30,37,6,75,98,39,90,63,74,52,92,64};
-
-    PartitionDisjointIntervalLeetcode915 ob = new PartitionDisjointIntervalLeetcode915();
-    System.out.println(ob.partitionDisjoint(nums));
-  }
 }
 /*
  * 
