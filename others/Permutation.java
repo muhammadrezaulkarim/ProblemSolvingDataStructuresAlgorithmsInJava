c318f21b08614fc3f6ffc66339c88f7faef29230
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2024-05-19 20:02:27 -0600

    Update Permutation.java

--- others/Permutation.java
+++ others/Permutation.java
@@ -1,53 +1,38 @@
-import java.util.Scanner;
-
 public class Permutation {
-    private static String inputStr;
-    private static Boolean[] flag;
-
+    private String inputStr;
+    private Boolean[] flag;
     private StringBuilder permutationHolder = new StringBuilder();
 
-    public static void main(String args[]) {
-        Scanner sc = null;
-        
-        try {
-            sc = new Scanner(System.in);
-            inputStr = sc.nextLine();
+    public void generationHelper(String str) {
+        inputStr = str;
+        flag = new Boolean[inputStr.length()];
 
-            flag = new Boolean[inputStr.length()];
-
-            for (int i = 0; i < flag.length; i++) {
-                flag[i] = false;
-            }
-
-            System.out.println();
-
-            Permutation permGenerator = new Permutation();
-
-            // requires to search for a sepecific string
-            // boolean found = false;
-
-            for (int i = 0; i < inputStr.length(); i++) {
-
-                permGenerator.permuteChar(i);
-
-                /*
-                 * // search for a sepcific string
-                 * found = permGenerator.findStringWithPermuation(i, "dbca");
-                 * 
-                 * if(found)
-                 * {
-                 * System.out.println("found");
-                 * break;
-                 * }
-                 */
-            }
-        } catch (Exception ex) {
-
-        } finally {
-            if (sc != null) {
-                sc.close();
-            }
+        // can be avoided as boolean default value is false
+        for (int i = 0; i < flag.length; i++) {
+            flag[i] = false;
         }
+
+        System.out.println();
+
+        // requires to search for a sepecific string
+        // boolean found = false;
+
+        for (int i = 0; i < inputStr.length(); i++) {
+
+            permuteChar(i);
+
+            /*
+             * // search for a sepcific string
+             * found = findStringWithPermuation(i, "dbca");
+             * 
+             * if(found)
+             * {
+             * System.out.println("found");
+             * break;
+             * }
+             */
+        }
+
     }
 
     public void permuteChar(int k) {
@@ -55,9 +40,7 @@ public class Permutation {
         flag[k] = true; // at the beginning set the flag to true
         permutationHolder.append(inputStr.charAt(k));
 
-        int currentLength = permutationHolder.toString().length();
-
-        if (currentLength == inputStr.length()) {
+        if (permutationHolder.toString().length() == inputStr.length()) {
             System.out.println(permutationHolder.toString());
 
             // before returning set the flag to false so that the symbol can be reached
@@ -66,7 +49,9 @@ public class Permutation {
 
             // backtrack: remove the character appended in this call to the current
             // permuation.
-            permutationHolder.delete(currentLength - 1, currentLength); // end index exclusive
+            // permutationHolder.delete(currentLength - 1, currentLength); // end index
+            // exclusive
+            permutationHolder.setLength(permutationHolder.length() - 1);
 
             return; // need to backtrack
         }
@@ -83,7 +68,7 @@ public class Permutation {
 
         // backtrack: remove the character appended in this call to the current
         // permuation.
-        permutationHolder.delete(currentLength - 1, currentLength); // end index exclusive
+        permutationHolder.setLength(permutationHolder.length() - 1); // end index exclusive
     }
 
     public boolean findStringWithPermuation(int k, String targetStr) {
@@ -91,9 +76,7 @@ public class Permutation {
         flag[k] = true; // at the beginning set the flag to true
         permutationHolder.append(inputStr.charAt(k));
 
-        int currentLength = permutationHolder.toString().length();
-
-        if (currentLength == inputStr.length()) {
+        if (permutationHolder.toString().length() == inputStr.length()) {
 
             // before returning set the flag to false so that the symbol can be reached
             // again in the search
@@ -102,13 +85,13 @@ public class Permutation {
             if (permutationHolder.toString().equals(targetStr)) {
                 // backtrack: remove the character appended in this call to the current
                 // permuation.
-                permutationHolder.delete(currentLength - 1, currentLength); // end index exclusive
+                permutationHolder.setLength(permutationHolder.length() - 1);
 
                 return true;
             } else {
                 // backtrack: remove the character appended in this call to the current
                 // permuation.
-                permutationHolder.delete(currentLength - 1, currentLength); // end index exclusive
+                permutationHolder.setLength(permutationHolder.length() - 1);
 
                 return false;
             }
@@ -129,8 +112,14 @@ public class Permutation {
 
         // backtrack: remove the character appended in this call to the current
         // permuation.
-        permutationHolder.delete(currentLength - 1, currentLength); // end index exclusive
+        permutationHolder.setLength(permutationHolder.length() - 1);
 
         return false;
+    }
+
+    public static void main(String[] args) {
+        String str = "abcd";
+        Permutation permGenerator = new Permutation();
+        permGenerator.generationHelper(str);
     }
 }
